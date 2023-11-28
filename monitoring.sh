341f0b69b2f6b3079f5eea37ceece1329b9576cb
hamada65 <elghayour65@gmail.com>
2023-11-28 11:15:16 -0500

    my script

---
+++ monitoring.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+###### [ Architecture ] #######
+
+arch=$(uname -a)
+
+###### [ vCpu ] ######
+
+vc=$(lscpu | grep "Socket(s)" | awk '{print $2}')
+
+###### [ CPU ] ######
+
+cp=$(lscpu | grep "Core(s) per socket" | awk '{print $NF}')
+
+###### [ Memory Usage ] ######
+
+mu=$(free -m | grep Mem | awk '{printf "%s/%sMB (%.2f%%)", $3, $2, $3/$2 * 100}')
+
+###### [ Disk Usage ] ######
+
+total_space=$(df -Bg | grep '^/dev' | grep -v '/boot$' | awk '{counter += $2} END {printf("%dGb", counter)}')
+used_space=$(df -Bm | grep '^/dev' | grep -v '/boot$' | awk '{counter += $3} END {printf("%d", counter)}')
+percentage_space=$(df -Bm | grep '^/dev' | grep -v '/boot$' | awk '{i += $2} {j += $3} END {printf("%d", j/i*100)}')
+
+###### [ CPU Load ] ######
+
+cl=$(vmstat 1 1 | awk 'NR==3 { printf "%.1f%%", 100 - $15}')
+
+###### [ Last Boot ] ######
+
+lb=$(who -b | awk '{print $3, $4}')
+
+###### [ LVM use ] ######
+
+lu=$(if [ $(lsblk | grep "lvm" | wc -l) -gt 0 ];then echo yes;else echo no; fi)
+
+###### [ Connections TCP ] ######
+
+tcp=$(ss -t | grep "ESTAB" | wc -l)
+
+###### [ User log ] ######
+
+ul=$(who | wc -l)
+
+###### [ Network ] ######
+
+netIP=$(hostname -I)
+mac=$(ip link | grep "link/ether" | awk '{printf("%s", $2)}')
+
+###### [ Sudo ] ######
+
+su=$(journalctl _COMM=sudo -q | grep 'COMMAND' | wc -l)
+
+wall "	#Architecture: $arch
+	#CPU physical : $vc
+	#vCpu : $cp
+	#Memory Usage: $mu
+	#Disk Usage: $used_space/$total_space ($percentage_space%)
+	#Cpu load: $cl
+	#Last boot: $lb
+	#LVM use: $lu
+	#Connections TCP : $tcp ESTABLISHED
+	#User log: $ul
+	#Network: IP $netIP ($mac)
+	#Sudo : $su cmd"
